c0735b7c4cbf64b7a4eae5dce4f915173d425fd6
202311012 <202311012@aau.edu.jo>
2026-07-04 16:07:10 +0300

    DataBaseConfig

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+};
+
+export default dbConfig;
